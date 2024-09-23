ec4b4667e90b1075a8935a00794df28d3205b827
aubairakif1998 <aubairakif98@gmail.com>
2024-09-23 20:55:50 +0500

    detaisls added for baording

--- src/utils/sendActivationEmail.ts
+++ src/utils/sendActivationEmail.ts
@@ -1,24 +1,26 @@
 import nodemailer from 'nodemailer';
-
 const sendActivationEmail = async (email: string, activationUrl: string) => {
+    // Create transporter object using Mailtrap SMTP
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: process.env.MAILTRAP_HOST,
+        port: Number(process.env.MAILTRAP_PORT),
         auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
+            user: process.env.MAILTRAP_USER,
+            pass: process.env.MAILTRAP_PASS,
         },
     });
 
     const mailOptions = {
-        from: process.env.EMAIL_USER,
+        from: '"no-reply@gonovatec.com>', // sender address
         to: email,
-        subject: 'Activate your account',
-        html: `<p>Please click the following link to activate your account and set your password:</p>
-               <a href="${activationUrl}">${activationUrl}</a>
-               <p>This link will expire in 24 hours.</p>`
+        subject: 'Activate Your Account', // Subject line
+        html: `
+            <p>Please click the following link to activate your account and set your password:</p>
+            <a href="${activationUrl}">${activationUrl}</a>
+            <p>This link will expire in 24 hours.</p>
+        `, // HTML body content
     };
-
+    // Send email
     await transporter.sendMail(mailOptions);
 };
-
 export default sendActivationEmail;
